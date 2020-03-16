7cdf6e723005aae2d5ee13e6ca8865b99fd8b3ec
Kong <43937364+KongBeycik@users.noreply.github.com>
2020-03-16 15:07:02 +0300

    Update saraç.py

--- saraç.py
+++ saraç.py
@@ -1,4 +1,26 @@
 ﻿'''
+ MIT License
+
+Copyright (c) 2020 Kong
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 önemli açıklama bu kod Kong tarafından yazılmıştır izin verilmeyen kişiler tarafından paylaşılması kesinlikte yasaktır
 nasıl çalıştırılır;
 python un kendi sitesinde python u indirin ardından cmd yi açın sonra dosya konumuna girin ardından python saraç.py yazın Not:Tokeni girmeyi unutmayın
